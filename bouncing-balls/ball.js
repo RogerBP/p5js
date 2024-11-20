938e29a9be13afe1af43d09d1ba05c04cd0e140a
Roger B. Pessoa <rogerbpessoa@gmail.com>
2024-11-20 10:40:43 -0300

    Many balls

--- bouncing-balls/ball.js
+++ bouncing-balls/ball.js
@@ -1,14 +1,16 @@
 class Ball {
     constructor(x, y, radius) {
         this.pos = createVector(x, y);
-        this.spd = createVector(5, 5);
+        this.spd = createVector(random(1, 15), random(1, 15));
+        this.spd.setMag(10);
         this.radius = radius;
+        this.color = { r: random(255), g: random(255), b: random(255) }
     }
     update() {
         this.pos.add(this.spd);
     }
     render() {
-        fill('yellow');
+        fill(this.color.r, this.color.g, this.color.b);
         noStroke();
         circle(this.pos.x, this.pos.y, this.radius * 2);
     }
--- bouncing-balls/sketch.js
+++ bouncing-balls/sketch.js
@@ -1,34 +1,82 @@
 let limit_circle;
-let ball;
-
+let balls = []
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
-  ball = new Ball(windowWidth / 2, windowHeight / 2, 10);
+  for (var i = 0; i < 1000; i++) {
+    balls.push(
+      new Ball(
+        // windowWidth / 2,
+        // windowHeight / 2,
+        random(300, windowWidth - 300),
+        random(300, windowHeight - 300),
+        10)
+    )
+  }
   limit_circle = {
     radius: width / 2,
     x: width / 2,
     y: height / 2,
+    angle: 0
   }
 }
 
 function draw() {
-  background(0);
-  circle(limit_circle.x, limit_circle.y, limit_circle.radius * 2)
-  ball.update();
-  check_limits(ball);
-  ball.render();
-
+  background(0, 0, 0);
+  draw_limit();
+  balls.forEach(ball => {
+    ball.update();
+    check_limits(ball);
+    ball.render();
+  });
   noFill();
   stroke(255);
+}
 
+function draw_limit() {
+  push();
+  translate(
+    limit_circle.x,
+    limit_circle.y
+  )
+  rotate(limit_circle.angle)
+  limit_circle.angle += 0.5;
+  stroke(0, 250, 0)
+  strokeWeight(10)
+  circle(0, 0, limit_circle.radius * 2)
+  stroke(250, 250, 0)
+  line(
+    0,
+    -limit_circle.radius,
+    0,
+    -limit_circle.radius + 10,
+  )
+  line(
+    0,
+    limit_circle.radius,
+    0,
+    limit_circle.radius - 10,
+  )
+  line(
+    - limit_circle.radius,
+    0,
+    - limit_circle.radius + 10,
+    0,
+  )
+  line(
+    limit_circle.radius,
+    0,
+    limit_circle.radius - 10,
+    0,
+  )
+  pop();
 }
 
 function check_limits(b) {
   const center = createVector(limit_circle.x, limit_circle.y);
   const dist = p5.Vector.dist(center, b.pos);
-  if (dist >= limit_circle.radius) {
+  if (dist >= limit_circle.radius - 20) {
     b.pos.sub(b.spd);
     b.spd.rotate(60);
   }
