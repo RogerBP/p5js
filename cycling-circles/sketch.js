fe742f99823ae4ccc8f261d55afc0b9125a8aa6c
Roger B. Pessoa <rogerbpessoa@gmail.com>
2024-11-30 10:03:16 -0300

    draw circle

--- cycling-circles/sketch.js
+++ cycling-circles/sketch.js
@@ -1,9 +1,22 @@
+let c = { x: 0, y: 0, r: 0, a: 0 }
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  const d = min(width, height);
+  c.x = width / 2;
+  c.y = height / 2;
+  c.r = d / 3;
+  c.a = 0;
 }
 
 function draw() {
   background(0);
-  fill('green')
-  circle(100, 100, 100);
+  drawCircle(c)
 }
+
+function drawCircle(c) {
+  noFill();
+  stroke(255);
+  strokeWeight(1);
+  circle(c.x, c.y, c.r * 2);
+} 
