3f4cfb283078b5991d8eb0b76258d595388aa873
Roger B. Pessoa <rogerbpessoa@gmail.com>
2024-11-30 11:11:17 -0300

    final effect

--- cycling-circles/sketch.js
+++ cycling-circles/sketch.js
@@ -2,9 +2,47 @@ let circles = []
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  const d = min(width, height);
-  let c = { x: width / 2, y: height / 2, r: d / 3, a: 0 }
-  circles.push(c)
+  const q = 20;
+  const size_base = min(width, height);
+  const size = size_base / q;
+
+  const diags = [
+  ]
+
+  for (let line = 0; line < q; line++) {
+    const s = (line + 1) / 100;
+    diags.push(
+      { cond: (x, y) => { return x === y + line }, s: s }
+    )
+    diags.push(
+      { cond: (x, y) => { return x + line === y }, s: s }
+    )
+  }
+
+  for (let line = 0; line < q; line++) {
+    for (let col = 0; col < q; col++) {
+      const x = col * size + size / 2;
+      const y = line * size + size / 2;
+      let s = random(0.01, 0.05);
+
+      diags.forEach(diag => {
+        const isDiag = diag.cond(col, line);
+        if (isDiag) {
+          s = diag.s;
+        }
+      })
+
+      let c = {
+        x: x,
+        y: y,
+        r: size / 3,
+        a: 0,
+        s: s
+      }
+      circles.push(c)
+    }
+  }
+
 }
 
 function draw() {
@@ -16,7 +54,7 @@ function drawCircle(c) {
   push();
   translate(c.x, c.y);
   rotate(c.a);
-  c.a += 0.01;
+  c.a += c.s;
   noFill();
   stroke(255);
   strokeWeight(1);
